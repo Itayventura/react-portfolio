4ad78020faf53d2cd619bd58299b3902ca2180f9
itayventura <ventura.itay@gmail.com>
2021-03-26 16:44:40 +0300

    adding a basic nav bar with links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact />
+      </Switch>
+    </Router>
       <h1>hello world</h1>
-    </div>
+    </>
   );
 }
 
