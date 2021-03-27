942e5258c624fb894a669cd550cbd93f420ae516
itayventura <ventura.itay@gmail.com>
2021-03-27 12:24:20 +0300

    last changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Projects from './components/pages/Projects';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -10,8 +13,11 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/projects' component={Projects} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
     </Router>
     </>
   );
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../App.css'
+
+function signUp() {
+    return (
+        <h1 className='sign-up'>SIGN UP</h1>
+    )
+}
+
+export default signUp
